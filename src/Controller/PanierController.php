094b6f46e2983331c6bb205f4c54f01d27e4b1c2
VvAlexandrevV <luis.alexandre@outlook.fr>
2026-04-07 14:44:03 +0200

    ajout: vider panier/ suppression ligne /compteur navbar/css temporaire/

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -10,6 +10,7 @@ use App\Repository\ProductRepository;
 
 final class PanierController extends AbstractController
 {
+    //fonction pour ajouter au panier
     #[Route('/panier/add/{id}', name: 'app_panier_add')]
     public function add(int $id, Request $request, ProductRepository $productRepository): Response
     {
@@ -37,22 +38,60 @@ final class PanierController extends AbstractController
         return $this->redirectToRoute('app_product');
     }
 
+    //fonction pour lire le panier
     #[Route('/panier', name: 'app_panier')]
     public function index(Request $request, ProductRepository $productRepository): Response
     {
         $panier = $request->getSession()->get('panier', []);
-        $produits = [];
+        $lignesPanier = [];
+        $total = 0;
 
-        foreach ($panier as $ligne) {
+        foreach ($panier as $index => $ligne) {
             $product = $productRepository->find($ligne['productId']);
 
             if ($product) {
-                $produits[] = $product;
+                $lignesPanier[] = [
+                    'index' => $index,
+                    'product' => $product,
+                ];
+
+                $total += $product->getPrix();
             }
         }
 
         return $this->render('panier/index.html.twig', [
-            'produits' => $produits,
+            'lignesPanier' => $lignesPanier,
+            'total' => $total,
         ]);
+}
+
+    //fonction pour vider le panier
+    #[Route('/panier/clear', name: 'app_panier_clear')]
+    public function clear(Request $request): Response
+{
+        $session = $request->getSession();
+        $session->remove('panier');
+
+        return $this->redirectToRoute('app_panier');
+}
+
+     //fonction pour supprimer un article
+    #[Route('/panier/remove/{index}', name: 'app_panier_remove')]
+    public function remove(int $index, Request $request): Response
+    {
+        $session = $request->getSession();
+
+        $panier = $session->get('panier', []);//recup le panier
+
+        if (isset($panier[$index])) { //verif que index existe
+            unset($panier[$index]);   //on supprime u e ligne
+
+            // reorganise le tableau
+            $panier = array_values($panier);
+
+            $session->set('panier', $panier);//on remet une session
+        }
+
+        return $this->redirectToRoute('app_panier');
     }
 }
